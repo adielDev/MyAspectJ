70414c125cb390ab26161b46d3b09740a7710238
adiel <adielbot@outlook.com>
2017-02-22 20:26:06 +0200

    ????

--- BombLoggerIntentService.java
+++ BombLoggerIntentService.java
@@ -1,8 +1,27 @@
 package adiel.myaspectj;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import myaspectj.filelogger.LocalLogManager;
+
 /**
  * Created by recntrek7 on 22/02/17.
  */
 
-public class BombLoggerIntentService {
+public class BombLoggerIntentService extends IntentService {
+
+
+    public BombLoggerIntentService() {
+        super("bomber");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        for (int i =0 ; i<100 ; i++){
+            //LocalLogManager.superLog("msg num "+i);
+            Log.d("adiel"," num:"+i);
+        }
+    }
 }
